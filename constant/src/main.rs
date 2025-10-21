1d9c4134a34766cd97f0471aeeffc0be2969aeaf
Renan Bernardelli <renanbernardelli@gmail.com>
2025-10-22 00:56:35 +0100

    adding folder constant

---
+++ constant/src/main.rs
@@ -0,0 +1,13 @@
+/**
+ * Uppercase by convention.
+ * Immutable by default.
+ * Data type is mandatory.
+ * Can be declared in any scope.
+ * Shadowing is not allowed.
+ */
+
+const URL: &str = "https://example.com";
+
+fn main() {
+    println!("{}", URL);
+}
